7aad931fd639b2d1ff3d316643fa4df25832a34b
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-24 21:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-window.promises = [];
+//window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-let promise1 = new Promise((resolve, reject) => {
-	queueMicrotask(resolve("Sukesh is a good boy"));
-})
-let promise2 = new Promise((resolve, reject) => {
-	reject("Sukesh is a bad boy");
-})
-let promise3 = new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve("Sukesh, You are going to next ruler of the world");
-	}, 1000);
-})
-let promise4 = new Promise((resolve, reject) => {
-	resolve("Sukesh, You are going to next ruler of the world");
-})
-let promise5 = new Promise((resolve, reject) => {
-	resolve("Sukesh, You are going to next ruler of the world");
-})
-let output = document.getElementById("output");
 
-Promise.any([promise1, promise2, promise3, promise4, promise5]).then((value) => {
-	output.innerText = value;
+let a = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(1)
+	} ,2000)
+})
+let b = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(2)
+	} , 3000)
+})
+let c = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(3)
+	} , 4000)
+})
+let d = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(4)
+	} , 5000)
+})
+let e = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve(5)
+	} , 1000)
+})
+
+let x = Promise.any([a,b,c,d ,e]);
+window.promises =[a,b,c,d ,e];
+x.then((data) => {
+	const elm = document.getElementById("output");
+	elm.innerText = data;
 })
