85f0eaac4b2b484595c9b759ba974dc0c53c7c33
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-24 21:25:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let promise1 = new Promise((resolve, reject) => {
+	queueMicrotask(resolve("Sukesh is a good boy"));
+})
+let promise2 = new Promise((resolve, reject) => {
+	reject("Sukesh is a bad boy");
+})
+let promise3 = new Promise((resolve, reject) => {
+	resolve("Sukesh, You are going to next ruler of the world");
+})
+let output = document.getElementById("output");
+
+Promise.any([promise1, promise2, promise3]).then((value) => {
+	output.innerText = value;
+})
