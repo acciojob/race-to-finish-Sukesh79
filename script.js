fa6d9bdc740f28d4127d5e9ab1832eb699312562
Sukesh Reddy <117748776+Sukesh79@users.noreply.github.com>
2023-08-24 21:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ let promise2 = new Promise((resolve, reject) => {
 let promise3 = new Promise((resolve, reject) => {
 	resolve("Sukesh, You are going to next ruler of the world");
 })
+let promise4 = new Promise((resolve, reject) => {
+	resolve("Sukesh, You are going to next ruler of the world");
+})
+let promise5 = new Promise((resolve, reject) => {
+	resolve("Sukesh, You are going to next ruler of the world");
+})
 let output = document.getElementById("output");
 
-Promise.any([promise1, promise2, promise3]).then((value) => {
+Promise.any([promise1, promise2, promise3, promise4, promise5]).then((value) => {
 	output.innerText = value;
 })
